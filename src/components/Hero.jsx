a1734bd4b18fb575523f7dab7bf5472773247c59
ThatDay1 <120332992+ThatDay1@users.noreply.github.com>
2024-04-24 21:44:42 +0300

    hero robot

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import Section from "./Section";
-import { curve } from "../assets";
+import { curve, heroBackground, robot } from "../assets";
+import Button from "./Button";
 
 const Hero = () => {
   return (
@@ -26,7 +26,38 @@ const Hero = () => {
               />
             </span>
           </h1>
-          <p></p>
+          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+            Unleash the power of AI within Brainwave. Upgrade your productivity
+            with Brainwave, the open AI chat app.
+          </p>
+          <Button href="/pricing" white>
+            Get started
+          </Button>
+        </div>
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+            <div className="relative bg-n-8 rounded-[1rem]">
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                <img
+                  src={robot}
+                  alt="robot"
+                  className="w-full"
+                  width={1024}
+                  height={490}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+            <img
+              src={heroBackground}
+              alt="heroBackground"
+              className="w-full"
+              width={1440}
+              height={1800}
+            />
+          </div>
         </div>
       </div>
     </Section>
