37beff1551fc78f429454f5f6ca13109c60fa922
ThatDay1 <120332992+ThatDay1@users.noreply.github.com>
2024-04-25 11:11:16 +0300

    hero section

--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -7,8 +7,8 @@ const Generating = ({ className }) => {
         className || ""
       } text-base`}
     >
-      <img src={loading} className="w-5 h-5 mr-4" alt="loading" />
-      AI is Generating
+      <img className="w-5 h-5 mr-4" src={loading} alt="Loading" />
+      AI is generating
     </div>
   );
 };
